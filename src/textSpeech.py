3009edbf4129e450f6577926849b347666dfce91
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2023-05-16 21:02:23 +0800

    fix text conversion

--- src/textSpeech.py
+++ src/textSpeech.py
@@ -4,14 +4,15 @@ import speech_recognition as sr
 # model = whisper.load_model("base.en")
 
 def listen() -> None:
-	r = sr.Recognizer()
-	with sr.Microphone() as source:
-		print("Say something!")
-		audio = r.listen(source)
+	while True: # Understandable text loop
+		r = sr.Recognizer()
+		with sr.Microphone() as source:
+			print("Say command")
+			audio = r.listen(source)
 
-	try:
-		return recognize_whisper(audio, language="english")
-	except sr.UnknownValueError:
-		Exception("Whisper could not understand audio")
-	except sr.RequestError as e:
-		Exception("Could not request results from Whisper, Error: \n", e)
+		try:
+			return r.recognize_whisper(audio, language="english")
+		except sr.UnknownValueError:
+			print("Whisper could not understand audio, please try again")
+		except sr.RequestError as e:
+			Exception("Could not request results from Whisper, Error: \n", e)
