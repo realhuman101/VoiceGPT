5e602d8cc1555717e92251fcf981b90e7278c8d2
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2023-05-16 21:53:22 +0800

    Rmv sphinx

--- src/textSpeech.py
+++ src/textSpeech.py
@@ -6,24 +6,17 @@ model = whisper.load_model("base.en")
 r = sr.Recognizer()
 
 def listen() -> str:
-	while True: # Understandable text loop
-		with sr.Microphone() as source:
-			print("Say command")
-			audio = r.listen(source)
+	with sr.Microphone() as source:
+		print("Say command")
+		audio = r.listen(source)
 
-		print("Recieved command...")
+	print("Recieved command...")
 
-		with open("audio.wav", "wb") as f:
-			f.write(audio.get_wav_data())
-		
-		print('Processing command...')
-		text = model.transcribe("audio.wav")["text"]
+	with open("audio.wav", "wb") as f:
+		f.write(audio.get_wav_data())
+	
+	print('Processing command...')
+	text = model.transcribe("audio.wav")["text"]
 
-		try:
-			# txt = r.recognize_sphinx(audio)
-			print("Interpreted Message: " + text + "\n\n\n")
-			return text
-		except sr.UnknownValueError:
-			print("Sphinx could not understand audio, please try again")
-		except sr.RequestError as e:
-			Exception("Could not request results from Sphinx, Error: \n", e)
+	print("Interpreted Message: " + text + "\n\n\n")
+	return text
